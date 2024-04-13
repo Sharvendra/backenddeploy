e35b7fe44a0d8434459e38b17c089c3333161784
Sharvendra <sharvendra2003@gmail.com>
2024-04-13 10:58:30 +0530

    data a added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
-
+const a=[1,2,3,4]
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,7 +14,10 @@ app.get('/login',(req,res)=>{
     res.send('<h1> please login at backend </h1>')
 })
 app.get('/youtube',(req,res)=>{
-    res.send('<h2>backend</h2>')
+     res.send('<h2>backend</h2>')
+})
+app.get('/github',(req,res)=>{
+    res.json(a)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
